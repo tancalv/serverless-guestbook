3778f7f235a18b6fbdcf197929c39ac411fde81d
tancalv <60224880+tancalv@users.noreply.github.com>
2022-01-25 12:43:29 -0500

    Update API URL

--- guestbook.js
+++ guestbook.js
@@ -1,7 +1,7 @@
 /**
  * Web application
  */
-const apiUrl = '';
+const apiUrl = 'https://d1934b55.us-south.apigw.appdomain.cloud/guestbook';
 const guestbook = {
   // retrieve the existing guestbook entries
   get() {
